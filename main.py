b37f2356c4bcc736ed828946d3f36a4fa6917eac
rumialunnar <rumialunnar@foxmail.com>
2019-08-11 12:05:06 +0800

    完善

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from flask import render_template,request
 import json
 import requests
 import urllib3
+from urllib.request import urlretrieve
+import base64
+import io
 app = Flask(__name__)
 
 
@@ -13,21 +16,25 @@ def index():
 
 @app.route('/get_cover', methods=['POST', 'GET'])
 def get_cover():
-    aid = request.values.get('aid')
-    url = 'https://api.bilibili.com/x/web-interface/view?aid=' + aid
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
-        'Referer':'https://www.bilbili.com'
-        }
-    urllib3.disable_warnings()
-    response = requests.get(url,headers=headers,verify=False).json()
-    code = response['code']
-    if code == 0:
-        data = response['data']
-        pic = data['pic']
-        return pic
+    if request.method == 'GET':
+        return 'ok'
     else:
-        return ('error message:',response['message'])
+        aid = request.values.get('aid')
+        url = 'https://api.bilibili.com/x/web-interface/view?aid=' + aid
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+            'Referer':'https://www.bilbili.com'
+            }
+        urllib3.disable_warnings()
+        response = requests.get(url,headers=headers,verify=False).json()
+        code = response['code']
+        if code == 0:
+            data = response['data']
+            pic_url = data['pic']
+            cover = urlretrieve(pic_url,'./static/cover')
+            return 'ok'
+        else:
+            return ('error message:',response['message'])
 
 
 if __name__ == '__main__':
